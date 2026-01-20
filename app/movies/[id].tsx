c077a430e5aa4f168e0efb9695f3478160823fcb
nooorulain <noorulannfatima@gmail.com>
2026-01-20 22:33:23 +0500

    routing & navigation

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+// dynamic route
+
+import React from "react";
+import { Text, View } from "react-native";
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>MovieDetails</Text>
+    </View>
+  );
+};
+
+export default MovieDetails;
